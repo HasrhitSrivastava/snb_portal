a9f81cd147a7946cb4dc493e33d8e51212cf59c1
HARSHIT SRIVASTAVA <imharshit.hs@gmail.com>
2025-12-29 17:24:43 +0530

    fix test issue

--- test/controllers/welcome_controller_test.rb
+++ test/controllers/welcome_controller_test.rb
@@ -2,7 +2,27 @@ require "test_helper"
 
 class WelcomeControllerTest < ActionDispatch::IntegrationTest
   test "should get index" do
-    get welcome_index_url
+    get root_url    
+    assert_response :success
+  end
+
+  test "should get contact" do
+    get contact_url
+    assert_response :success
+  end
+
+  test "should get discover_more" do
+    get discover_more_url
+    assert_response :success
+  end
+
+  test "should get admission" do
+    get admission_url
+    assert_response :success
+  end
+
+  test "should get academic" do
+    get academic_url
     assert_response :success
   end
 end
